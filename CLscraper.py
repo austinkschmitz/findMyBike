0e6eec7d28b36dab34fabee8b42aa8f51e79c0bc
austinkschmitz <35646306+austinkschmitz@users.noreply.github.com>
2021-11-22 17:15:28 -0700

    Create CLscraper.py
    init

---
+++ CLscraper.py
@@ -0,0 +1,80 @@
+import requests
+import time
+# https://requests.readthedocs.io/en/master/
+from bs4 import BeautifulSoup
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+import smtplib
+# Import the email modules we'll need
+from email.message import EmailMessage
+
+# Get list of offers
+# https://boise.craigslist.org/search/mca?query=yamaha+raider&purveyor-input=all&search_distance=500&postal=83701
+###
+SLC = 'https://saltlakecity.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+Boise = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&max_price=20000'
+Reno = 'https://reno.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+Portland = 'https://portland.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+Las_Vegas = 'https://lasvegas.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+
+URLS = [SLC, Boise, Reno, Portland, Las_Vegas]
+
+def find_bike(URL):
+    #URL = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&postal=83701&max_price=20000'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_='rows')
+    car_elems = results.find_all('li', class_='result-row')
+    # Parse each offer tile
+    for car_elem in car_elems:
+        price_elem = car_elem.find('span', class_='result-price')
+        #url_elem = car_elem.find('a', class_="result-image gallery")['href']
+        title_elem = car_elem.find('a', class_='result-title hdrlnk')
+
+        #print(title_elem.text.strip())
+        #print(price_elem.text.strip())
+        #print()
+        if title_elem.text.strip().lower().find('bmw') != -1:
+            print('FOUND YOU BITCH')
+            print(car_elem)
+            url_elem = car_elem.find('a', class_="result-image gallery")['href']
+            print(url_elem)
+
+            # Get details of particular offer
+            CAR_URL = url_elem
+            car_page = requests.get(CAR_URL)
+            car_soup = BeautifulSoup(car_page.content, 'html.parser')
+            attributes = car_soup.find_all('p', class_='attrgroup')
+
+            # Get attributes
+            for attribute in attributes:
+                spans = attribute.find_all('span')
+                for span in spans:
+                    text = span.text.strip()
+                    print(text)
+                    print(text.split(':'))
+                    print()
+                    # Get offer description
+                    posting_body = car_soup.find('section', {"id": "postingbody"})
+                    print(posting_body.text)
+                    # Open the plain text file whose name is in textfile for reading.
+
+
+for URL in URLS:
+    find_bike(URL)
+def test():
+    with open(textfile) as fp:
+        # Create a text/plain message
+        msg = EmailMessage()
+        msg.set_content(fp.read())
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = f'The contents of {textfile}'
+    msg['From'] = me
+    msg['To'] = you
+
+    # Send the message via our own SMTP server.
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
