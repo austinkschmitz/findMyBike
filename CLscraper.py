1801b312a76594e3755bad354c4c9ef2e474da79
austinkschmitz <35646306+austinkschmitz@users.noreply.github.com>
2021-11-23 18:10:28 -0700

    Update CLscraper.py
    added try/catch

--- CLscraper.py
+++ CLscraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import sys
 # https://requests.readthedocs.io/en/master/
 from bs4 import BeautifulSoup
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
@@ -18,49 +19,53 @@ Las_Vegas = 'https://lasvegas.craigslist.org/search/mca?purveyor-input=all&searc
 
 URLS = [SLC, Boise, Reno, Portland, Las_Vegas]
 
-def find_bike(URL):
-    #URL = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&postal=83701&max_price=20000'
-    page = requests.get(URL)
+
+def find_bike(url):
+    # URL = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&postal=83701&max_price=20000'
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find(class_='rows')
     car_elems = results.find_all('li', class_='result-row')
     # Parse each offer tile
     for car_elem in car_elems:
         price_elem = car_elem.find('span', class_='result-price')
-        #url_elem = car_elem.find('a', class_="result-image gallery")['href']
+
         title_elem = car_elem.find('a', class_='result-title hdrlnk')
 
-        #print(title_elem.text.strip())
-        #print(price_elem.text.strip())
-        #print()
+        # print(title_elem.text.strip())
+        # print(price_elem.text.strip())
+        # print()
         if title_elem.text.strip().lower().find('bmw') != -1:
             print('FOUND YOU BITCH')
-            print(car_elem)
-            url_elem = car_elem.find('a', class_="result-image gallery")['href']
-            print(url_elem)
+            try:
+                url_elem = car_elem.find('a', class_="result-image gallery")['href']
+                print(url_elem)
+                # Get details of particular offer
+                CAR_URL = url_elem
+                car_page = requests.get(CAR_URL)
+                car_soup = BeautifulSoup(car_page.content, 'html.parser')
+                attributes = car_soup.find_all('p', class_='attrgroup')
+                
+                # Get attributes
+                for attribute in attributes:
+                    spans = attribute.find_all('span')
+                    for span in spans:
+                        text = span.text.strip()
+                        print(text)
+                        print(text.split(':'))
+                        print()
+                        # Get offer description
+                        posting_body = car_soup.find('section', {"id": "postingbody"})
+                        print(posting_body.text)
+                        # Open the plain text file whose name is in textfile for reading.
+            except:
+                url_elem = 'UNABLE TO GET LINK!!!!!!'
 
-            # Get details of particular offer
-            CAR_URL = url_elem
-            car_page = requests.get(CAR_URL)
-            car_soup = BeautifulSoup(car_page.content, 'html.parser')
-            attributes = car_soup.find_all('p', class_='attrgroup')
 
-            # Get attributes
-            for attribute in attributes:
-                spans = attribute.find_all('span')
-                for span in spans:
-                    text = span.text.strip()
-                    print(text)
-                    print(text.split(':'))
-                    print()
-                    # Get offer description
-                    posting_body = car_soup.find('section', {"id": "postingbody"})
-                    print(posting_body.text)
-                    # Open the plain text file whose name is in textfile for reading.
+for url in URLS:
+    find_bike(url)
 
 
-for URL in URLS:
-    find_bike(URL)
 def test():
     with open(textfile) as fp:
         # Create a text/plain message
@@ -77,4 +82,3 @@ def test():
     s = smtplib.SMTP('localhost')
     s.send_message(msg)
     s.quit()
-
