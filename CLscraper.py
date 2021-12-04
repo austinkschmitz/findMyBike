9a9e0c429329d1969f1be87cdcf3b016dc5d8fd0
austinkschmitz <35646306+austinkschmitz@users.noreply.github.com>
2021-12-04 14:11:52 -0500

    Update CLscraper.py

--- CLscraper.py
+++ CLscraper.py
@@ -1,6 +1,8 @@
-import requests
-import time
-import sys
+# TODO
+# Create dependencies
+
+
+import requests, time, sys, schedule
 # https://requests.readthedocs.io/en/master/
 from bs4 import BeautifulSoup
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
@@ -11,16 +13,31 @@ from email.message import EmailMessage
 # Get list of offers
 # https://boise.craigslist.org/search/mca?query=yamaha+raider&purveyor-input=all&search_distance=500&postal=83701
 ###
-SLC = 'https://saltlakecity.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
-Boise = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&max_price=20000'
-Reno = 'https://reno.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
-Portland = 'https://portland.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
-Las_Vegas = 'https://lasvegas.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+# SLC = 'https://saltlakecity.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+SLC = 'saltlakecity'
+# Boise = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&max_price=20000'
+Boise = 'boise'
+# Reno = 'https://reno.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+Reno = 'reno'
+# Portland = 'https://portland.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+Portland = 'portland'
+# Las_Vegas = 'https://lasvegas.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000'
+Las_Vegas = 'lasvegas'
 
-URLS = [SLC, Boise, Reno, Portland, Las_Vegas]
+Cities = [SLC, Boise, Reno, Portland, Las_Vegas]
+URLS = []
+Parts = []
+keywords = ['XV1900A', 'raider']
+
+for city in Cities:
+    URLS.append(
+        'https://' + city + '.craigslist.org/search/mca?purveyor-input=all&search_distance=1000&max_price=20000')
+    Parts.append('https://' + city + '.craigslist.org/search/mpa?purveyor-input=all')
+    # 'https://boise.craigslist.org/search/mpa?purveyor-input=all'
+    # 'https://boise.craigslist.org/search/mpa?query=raider&purveyor-input=all'
 
 
-def find_bike(url):
+def find_bike(url, keyword):
     # URL = 'https://boise.craigslist.org/search/mca?purveyor-input=all&search_distance=500&postal=83701&max_price=20000'
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -35,7 +52,7 @@ def find_bike(url):
         # print(title_elem.text.strip())
         # print(price_elem.text.strip())
         # print()
-        if title_elem.text.strip().lower().find('bmw') != -1:
+        if title_elem.text.strip().lower().find(keyword) != -1:
             print('FOUND YOU BITCH')
             try:
                 url_elem = car_elem.find('a', class_="result-image gallery")['href']
@@ -45,7 +62,7 @@ def find_bike(url):
                 car_page = requests.get(CAR_URL)
                 car_soup = BeautifulSoup(car_page.content, 'html.parser')
                 attributes = car_soup.find_all('p', class_='attrgroup')
-                
+
                 # Get attributes
                 for attribute in attributes:
                     spans = attribute.find_all('span')
@@ -62,8 +79,14 @@ def find_bike(url):
                 url_elem = 'UNABLE TO GET LINK!!!!!!'
 
 
-for url in URLS:
-    find_bike(url)
+def start_job():
+    for url in URLS:
+        find_bike(url, keywords[0])
+        find_bike(url, keywords[1])
+
+    for part in Parts:
+        find_bike(part, keywords[0])
+        find_bike(part, keywords[1])
 
 
 def test():
@@ -82,3 +105,12 @@ def test():
     s = smtplib.SMTP('localhost')
     s.send_message(msg)
     s.quit()
+
+
+#schedule.every(12).hours.do(start_job())
+while True:
+#    schedule.run_pending()
+    start_job()
+    time.sleep(42300)
+
+
